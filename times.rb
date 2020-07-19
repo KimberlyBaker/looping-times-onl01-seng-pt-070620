7bc9be16e9d5e3dd768f861e96d43d8fad052bd4
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 22:23:36 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,5 @@
 def using_times
-  3.times do 
-    puts "Hiding 10 stolen jewels."
-    jewels_in_bag = jewels_in_bag - 10
-    puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+  7.times do 
+    puts "Wingardium Leviosa"
   end
 end
