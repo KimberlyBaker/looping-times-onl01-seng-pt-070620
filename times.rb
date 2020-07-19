0e92de2778cfdda36805bfc694b9714a334e6e70
Kimberly Baker <bakerk2010@gmail.com>
2020-07-19 22:22:44 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -3,6 +3,5 @@ def using_times
     puts "Hiding 10 stolen jewels."
     jewels_in_bag = jewels_in_bag - 10
     puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+  end
 end
- 
-puts "We have #{jewels_in_bag} jewels still to hide!"
